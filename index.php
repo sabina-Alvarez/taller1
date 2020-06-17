4632042d1ae2eb274eefb5bb38baf346394c46bb
sabina-Alvarez <sabinamonsalve71@gmail.com>
2020-06-16 21:52:30 -0500

    momento1 taller

---
+++ index.php
@@ -0,0 +1,94 @@
+<?php
+/*$numero1=20;
+$numero2=50;
+$suma=$numero1+$numero2;
+echo("La suma de: ".$numero1."+".$numero2."=".$suma);
+echo("<br>");
+$resta1= 20;
+$resta2= 7;
+$resta=$resta1-$resta2;
+echo('La resta de: ' .$resta1."-" .$resta2. "=" .$resta);
+echo("<br>");
+$multiplo1= 10;
+$multiplo2= 20;
+$multiplo= $multiplo1 * $multiplo2;
+echo('Se multiplico: ' .$multiplo1. "*" .$multiplo2. "=" .$multiplo);
+echo("<br>");
+$division1= 10;
+$division2= 5;
+$division= $division1 / $division2;
+echo('Se dividio : ' .$division1. "/" .$division2. "=" .$division);*/
+/////////////////////////////////////////////////////////////////////////
+/*$peso= 85;
+$altu= 1.78;
+$imc = $peso / ($altu * $altu);
+$altura= $altu/100;
+
+if ($peso >= 40){
+  echo('Obesidad tipo III:' .$imc);
+}
+else if ($peso >= 35 && $peso<= 39.9){
+  echo('Obesidad tipo II:' .$imc);
+}
+else if($peso >= 30 && $peso <= 34.9){
+  echo('Obesidad tipo I:' .$imc);
+}
+else if($peso >= 25 && $peso <=29.9){
+  echo('Sobrepeso:' .$imc);
+}
+else if($peso >= 18.5 && $peso <=24.9  ){
+  echo('Peso normal:' .$imc);
+}
+else if($peso <= 18.4){
+  echo('Insuficiencia de peso:' .$imc);
+}*/
+//////////////////////////////////////////////////////////////////7
+/*$cantidadZapatos = 5;
+$costoneto = 150000;
+$totalCompra = ($cantidadZapatos * $costoneto);
+if ($cantidadZapatos == 3) {
+  $valordescuento = $totalCompra * 0.10;    
+} 
+else if ($cantidadZapatos > 3 && $cantidadZapatos <=5) {
+  $valordescuento = $totalCompra * 0.20;
+} 
+else if ($cantidadZapatos > 5) {
+  $valordescuento = $totalCompra * 0.40;
+}
+$valorPagar = ($cantidadZapatos * $costoneto) - $valordescuento;
+echo('Cantidad Vendida: '.$cantidadZapatos. '<br> Costo Neto: ' .$totalCompra. '<br> Valor del Descuento: '.$valordescuento.  '<br> Valor de un Pagar es: ' .$valorPagar);*/
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*$valorhora= 20000;
+$valorhoraE=25000;
+$horaTrabajada= 36;
+$total= ($valorhora * $horaTrabajada);
+$pago=($valorhoraE * $horaTrabajada);
+if ($horaTrabajada >= 40){
+  echo('Total pago:' .$pago);
+}
+else if ($horaTrabajada <= 40){
+  echo('Total pago:'.$total);
+}*/
+////////////////////////////////////////////////////////////////////////////////
+/*Después de realizar una consulta en la base de datos DE LA JUGOSA SAS se tienen almacenados los valores de dicha 
+consulta en un arreglo denominado salpicón,
+ el cual presenta la siguiente configuración:
+[dulce1]=>”Banano”  [dulce2]=>”Manzana”  [dulce3]=>”Durazno”  [acido1]=>”Piña”  [acido2]=>”Naranja”  [acido3]=>”Lulo”*/
+
+/*$salpicon=array(
+    "dulce1"=>array("banano"),
+    "dulce2"=>array("manzana"),
+    "dulce3"=>array("durazno"),   
+    "acido1"=>array("piña"),
+    "acido2"=>array("naranja"),
+    "acido3"=>array("lulo"),
+);
+
+ foreach($salpicon as $clave=>$valor){
+
+    echo($clave);
+    echo("<br>");
+    print_r($valor);
+    echo("<br>");
+}*/
+?>
